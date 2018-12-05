6ca5f10d11adf2db6c72bf5e93aa27ce87415ac2
JakubKordel <45512372+JakubKordel@users.noreply.github.com>
2018-12-05 20:13:11 +0100

    Update funkcje.h

--- funkcje.h
+++ funkcje.h
@@ -2,21 +2,24 @@
 #include "podst.h"
 
 typedef struct Osoba{
+    int id_o;
     char imie[20];
     char nazwisko[20];
     struct Osoba* nast;
 }Osoba;
 
 typedef struct Przedmiot{
-    unsigned int id_prz;
+    id_p;
     char nazwa[20];
+    struct Przedmiot* nast;
 }Przedmiot;
 
 Osoba* dodajOsobe( Osoba* glowa, char imie[20], char nazwisko[20] );
-
 Osoba* wczytajOsobe( Osoba* glowaOsoba );
+Przedmiot* dodajPrzedmiot( Przedmiot* glowaPrzedmiot, char nazwa[20] );
+Przedmiot* wczytajPrzedmiot( Przedmiot* glowaPrzedmiot );
 void wyswietlListeOsob( Osoba* glowaOsoba );
-
+void wyswietlListePrzedmiotow( Przedmiot* glowaPrzedmiot );
 
 
 
