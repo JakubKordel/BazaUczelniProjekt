b8ea6e80152b1e331377de43a22fb6adf4e221b7
JakubKordel <45512372+JakubKordel@users.noreply.github.com>
2018-12-05 20:25:02 +0100

    Update funkcje.c

--- funkcje.c
+++ funkcje.c
@@ -1,75 +1,24 @@
-#include "funkcje.h"
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+#include "podst.h"
 
-Osoba* dodajOsobe( Osoba* glowa, char im[20], char nazw[20] )
-{
-    Osoba* nowaOsoba = malloc( sizeof( Osoba ) );
-    strcpy ( nowaOsoba->imie, im );
-    strcpy ( nowaOsoba->nazwisko, nazw );
-    nowaOsoba->nast = glowa;
-    return nowaOsoba;
-}
-
-
-Osoba* wczytajOsobe( Osoba* glowaOsoba )
-{
+typedef struct Osoba{
+    int id_o;
     char imie[20];
     char nazwisko[20];
-    char napis[20];
-    printf("Podaj imie: ");
-    strcpy ( imie, wczytajNapis(napis) );
-    printf("Podaj nazwisko: ");
-    strcpy ( nazwisko, wczytajNapis( napis ) );
-    glowaOsoba = dodajOsobe(glowaOsoba, imie, nazwisko);
-    return glowaOsoba;
-}
+    struct Osoba* nast;
+}Osoba;
 
-Przedmiot* dodajPrzedmiot( Przedmiot* glowa, char nazw[30] )
-{
-    Przedmiot* nowyPrzedmiot = malloc( sizeof( Przedmiot ) );
-    strcpy ( nowyPrzedmiot -> nazwa, nazw );
-    nowyPrzedmiot->nast = glowa;
-    return nowyPrzedmiot;
-}
-
-Przedmiot* wczytajPrzedmiot( Przedmiot* glowaPrzedmiot )
-{
+typedef struct Przedmiot{
+    int id_p;
     char nazwa[20];
-    char napis[20];
-    printf("Podaj nazwe przedmiotu: ");
-    strcpy ( nazwa, wczytajNapis( napis ) );
-    glowaPrzedmiot = dodajPrzedmiot( glowaPrzedmiot, nazwa );
-    return glowaPrzedmiot;
-}
+    struct Przedmiot* nast;
+}Przedmiot;
 
-void wyswietlListeOsob( Osoba* glowaOsoba )
-{
-    Osoba* x = glowaOsoba;
-    while( x )
-    {
-        printf( "%s %s\n", x->imie, x->nazwisko );
-        x = x->nast;
-    }
-}
-
-void wyswietlListePrzedmiotow( Przedmiot* glowaPrzedmiot )
-{
-    Przedmiot* x = glowaPrzedmiot;
-    while( x )
-    {
-        printf( "%s \n", x->nazwa );
-        x = x->nast;
-    }
-}
-
-void zwolnij( Osoba* glowa )
-{
-    Osoba* poprzedni=NULL;
-    while( glowa )
-    {
-        poprzedni = glowa;
-        glowa = glowa -> nast;
-        free( poprzedni );
-    }
-}
+Osoba* dodajOsobe( Osoba* glowa, char imie[20], char nazwisko[20] );
+Osoba* wczytajOsobe( Osoba* glowaOsoba );
+Przedmiot* dodajPrzedmiot( Przedmiot* glowaPrzedmiot, char nazwa[20] );
+Przedmiot* wczytajPrzedmiot( Przedmiot* glowaPrzedmiot );
+void wyswietlListeOsob( Osoba* glowaOsoba );
+void wyswietlListePrzedmiotow( Przedmiot* glowaPrzedmiot );
+void zwolnijOsoba( Osoba* glowa );
+void zwolnijPrzemdiot( Przedmiot* glowa );
